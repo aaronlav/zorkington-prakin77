e05697c130dc15bb3b170f5507a32f7767812318
prakin77 <58230098+prakin77@users.noreply.github.com>
2020-02-14 12:00:25 -0500

    few more classes added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  *    Date: 2/14/2020
  */
 
-// readline code
+// readline code-------------------------
 const readline = require("readline");
 const readlineInterface = readline.createInterface(
   process.stdin,
@@ -17,4 +17,69 @@ function ask(questionText) {
     readlineInterface.question(questionText, resolve);
   });
 }
-// end readline code
+// end readline code------------------------
+
+// setting up the game classes & constructors
+class Gamestate {
+  constructor(userName) {
+    this.currentRoom = rooms.room1;
+    this.user = new Player(userName);
+  }
+
+  moveToRoom(nextRoom) {
+    this.currentRoom = rooms[nextRoom];
+  }
+
+  printState() {
+    console.log(this);
+  }
+}
+
+// setting up Room classes & constructor
+class Room {
+  constructor(roomName, description, inventory, isLocked) {
+    this.roomName = roomName;
+    this.description = description[this.roomDescriptions];
+    this.inventory = inventory || [];
+    this.isLocked = isLocked(true, false);
+  }
+
+  addInventory(item) {
+    this.inventory.push(item);
+  }
+  removeInventory(item) {
+    this.inventory.splice(this.inventory.indexOf(item), 1)
+  }
+  showInventory() {
+    console.log("Here you find: " + this.inventory);
+  }
+  unlockDoor(input){
+    let passowrd = "00000"
+    if (input.includes(password)){
+      return true;
+    }
+  }
+}
+
+//Setting up Player class and constructor
+class Player {
+  constructor(name, inventory){
+    this.name = name;
+    this.inventory=inventory || [];
+  }
+checkName(){
+  return this.name;
+}
+getInventory(item){
+  global.game.currentRoom.removeInventory(item);
+  this.inventory.push(item);
+}
+dropInventory(item){
+  this.inventory.splice(this.inventory.indexOf(item), 1);
+  global.game.currentRoom.addInventory(item);
+}
+showInventory(){
+  console.log("You currently have "+ this.inventory);
+}
+}
+
