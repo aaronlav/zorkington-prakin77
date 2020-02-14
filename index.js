030d9259c06faf8af2855bc40dbb18841c277ae7
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-02-14 13:44:17 -0500

    added room construction

--- index.js
+++ index.js
@@ -25,48 +25,48 @@ constructor(name, description, inventory, isLocked) {
   }
 }
 
-let room1 = new Room()
+
+
+// let rooms = 
+// [room1,
+// room2,
+// room3,
+// room4,
+// room5,
+// room6,
+// room7,
+// room8,
+// room9,
+// room10]
+
 
 let roomNames = {
-  room1: '182 Main',
+  room1: '182 Zork Street',
   room2:  "Foyer",
-  room3: "Classroom"
-  room4: "Corner of Main and Zorkooski",
-  room5: "North Zorkooski Ave",
+  room3: "Classroom",
+  room4: "Corner of Zork Street and Zorkooski Avenue",
+  room5: "North Zorkooski Avenue",
   room6: "City Market",
-  room7: "West Main Street",
+  room7: "West Zork Street",
   room8: "Kountry Zork Deli",
-  room9: "South Zorkooski Ave",
+  room9: "South Zorkooski Avenue",
   room10: "Zorkplain Farms",
 }
 
 let roomDescriptions = {
-  room1: 'You are in front of 182 Main Street, Zorkington, Zorkmont. You are in the center of town. Main Street runs either direction east and west. The entrance to Zorkington Code Academy is directly to your North. There is a sign in the window',
-  room2: 'You are in the foyer of 182 Main St, the classroom of Zorkington Code Academy is farther down to the North. You see your instructor, Bob, sitting fast asleep outside the classroom.',
+  room1: 'You are in front of 182 Zork Street, Zorkington, Zorkmont. You are in the center of town. Zork Street runs either direction east and west. The entrance to Zorkington Code Academy is directly to your North. There is a sign in the window',
+  room2: 'You are in the foyer of 182 Zork Street, the classroom of Zorkington Code Academy is farther down to the North. You see your instructor, Bob, sitting fast asleep outside the classroom.',
   room3: 'You are in a moderately sized classroom with some tables and chairs. A small whiteboard is at the front with some leftover work from yesterdays lesson.',
-  room4: 'You are at the corner where Main Street intersects Zorkooski Avenue. Zorkington Code Academy is to your West down Main Street, and you can see Zorkington City Market to your North, and Zorkplain Farms to your south on Zorkooski Ave.',
+  room4: 'You are at the corner where Zork Street intersects Zorkooski Avenue. Zorkington Code Academy is to your West down Zork Street, and you can see Zorkington City Market to your North, and Zorkplain Farms to your south on Zorkooski Ave.',
   room5: 'You are in front of Zorkington City Market on North Zorkooski Ave. It is a relatively large grocery store with a strange mixture of both rich and homeless people going in and out. There is a sign on the front of the building',
   room6: 'You are inside of Zorkington City Market. It smells weird. Amoung many strange products, you see a large display on one side of the store with many different kinds of tea.',
-  room7: 'You are west of Zorkington Code Academy on Main Street, Kountry Zork Deli lies to your South. The delicious smell of bacon and sausage waft out of the kitchen to you. There is a sign in the window',
+  room7: 'You are west of Zorkington Code Academy on Zork Street, Kountry Zork Deli lies to your South. The delicious smell of bacon and sausage waft out of the kitchen to you. There is a sign in the window',
   room8: 'You are inside of Kountry Zork Deli, you see a peak of some fantastic meats sizziling on the flatop in the kitchen in the back. An employee is at the counter in front of you.',
   room9: 'You are in front of Zorkplain Farms, a small gas station and convenience store on South Zorkooski Ave. There is a sign in the window.',
   room10: 'You are inside of Zorkplain Farms. There are a couple aisles with snacks and drinks and a large beer cooler.',
 }
 
-let roomInventory = {
-  room1: [sign1],
-  room2: ['Bob','Tea Station','Inferior Tea'],
-  room3: ['chair'],
-  room4: null,
-  room5:[sign2],
-  room6: ["Fair Trade Certified Organic Irish Breakfast Tea", "Fair Trade Certified Organic Scottish Breakfast Tea", 'Unfair Trade British Tea', 'Slave Labor Certified "Food" Grade Leaf Product'],
-  room7:[sign3],
-  room8: [ 'Large Two Meat Shiner with Bacon and Sausage', 'Small Two Meat Shiner with Bacon and Sausage', 'Large One Meat Shiner with Bacon', 'Large One Meat Shiner with Sausage'],
-  room9:[sign4],
-  room10: ["Bob's Keys", 'Long Trail Brewing Company Trail Hopper IPA'],
-}
-
-let roomLocked = {
+let roomIsLocked = {
   room1: false,
   room2: false,
   room3: true,
@@ -79,10 +79,54 @@ let roomLocked = {
   room10: true,
 }
 
-let signText = {
-  sign1: '',
-  sign2: '',
-  sign3: '',
-  sign4: '',
+// let signText = {
+//   sign1: 'Zorkington Code Academy: Learn the Meaning of Life, the Universe, and Everything!',
+//   sign2: 'Zorkington City Market: Your Community Store for all you Fair Trade and Certified Organic needs!',
+//   sign3: 'Kountry Zork: Home of the Zorker',
+//   sign4: 'Zorkplain Farms: All Zork Trail products on sale',
 
+// }
+
+let sign1 = {
+  'Zorkington Code Academy: Learn the Meaning of Life, the Universe, and Everything!'
 }
+
+let sign2 = {
+  'Zorkington City Market: Your Community Store for all you Fair Trade and Certified Organic needs!'
+}
+
+let sign3 = {
+  'Kountry Zork: Home of the Zorker'
+}
+
+let sign4 = {
+  'Zorkplain Farms: All Zork Trail products on sale!'
+}
+
+let roomInventory = {
+  room1: [sign1],
+  room2: ['Bob','Tea Station','Inferior Tea'],
+  room3: ['chair'],
+  room4: null,
+  room5:[sign2],
+  room6: ["Fair Trade Certified Organic Zork Breakfast Tea", "Fair Trade Certified Organic  Tea", 'Unfair Trade British Tea', 'Slave Labor Certified "Food" Grade Leaf Product'],
+  room7:[sign3],
+  room8: [ 'Large Two Meat Zorker with Bacon and Sausage', 'Small Two Meat Zorker with Bacon and Sausage', 'Large One Meat Zorker with Bacon', 'Large One Meat Zorker with Sausage'],
+  room9:[sign4],
+  room10: ["Bob's Keys", 'Zork Trail Brewing Company Zork Hopper IPA'],
+}
+
+let room1 = new Room(roomNames[room1], roomDescriptions[room1], roomInventory[room1], roomIsLocked[room1])
+let room2 = new Room(roomNames[room2], roomDescriptions[room2], roomInventory[room2], roomIsLocked[room2])
+let room3 = new Room(roomNames[room3], roomDescriptions[room3], roomInventory[room3], roomIsLocked[room3])
+let room4 = new Room(roomNames[room4], roomDescriptions[room4], roomInventory[room4], roomIsLocked[room4])
+let room5 = new Room(roomNames[room5], roomDescriptions[room5], roomInventory[room5], roomIsLocked[room5])
+let room6 = new Room(roomNames[room6], roomDescriptions[room6], roomInventory[room6], roomIsLocked[room6])
+let room7 = new Room(roomNames[room7], roomDescriptions[room7], roomInventory[room7], roomIsLocked[room7])
+let room8 = new Room(roomNames[room8], roomDescriptions[room8], roomInventory[room8], roomIsLocked[room8])
+let room9 = new Room(roomNames[room9], roomDescriptions[room9], roomInventory[room9], roomIsLocked[room9])
+let room10 = new Room(roomNames[room10], roomDescriptions[room10], roomInventory[room10], roomIsLocked[room10])
+
+// for(let room of rooms){
+  
+// }
